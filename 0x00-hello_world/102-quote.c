519327983c35b3beba154177b365134cfb6a73f7
rediet-92 <rmersha92@gggmail.com>
2022-06-03 10:05:47 -0700

    red

---
+++ 0x00-hello_world/102-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - writes out the values
+ * Description: avails the tru values out.
+ * Return: prints out 1
+ */
+int main(void)
+{ FILE * fp;
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+fp = fopen("file.txt", "w");
+fwrite(str, 2, sizeof(str), fp);
+fclose(fp);
+return (1);
+}
